9c2c05f1a3e2833431362afc5d008d3112484a59
Joshua Powers <josh.powers@canonical.com>
2017-11-30 12:33:37 -0800

    Addin configs
    Adds various simple configs doing various things with netplan.
    
    Also adds in timing of runs

--- netplan_test/runner.py
+++ netplan_test/runner.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """Central runner for framework."""
+import logging
+import time
+
 from .images import BaseImage, TestImage
 from .utils import find_pristine_image, find_tests, setup_results_dir
 
@@ -12,15 +15,19 @@ def collect(release, image, tests, results_dir):
     @param tests: optional, specify specific tests to run
     @param results_dir: optional, path to store results
     """
+    log = logging.getLogger('netplan_test')
     results_dir = setup_results_dir(results_dir)
     base_image_path = find_pristine_image(release, image)
     base_image = BaseImage(base_image_path, results_dir)
 
     tests = find_tests(tests)
     for test_yaml_path in tests:
+        start_time = time.time()
         test_image = TestImage(base_image, test_yaml_path)
         test_image.launch()
         test_image.collect()
+        end_time = time.time()
+        log.info('took %s seconds', round(end_time - start_time, 2))
 
 
 def run(release, image, tests, results_dir):
